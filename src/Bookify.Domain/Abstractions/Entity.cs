13a4a58a201e0f021c347edb0358c583b2c9781e
alnaimi-github <alnymybdalwas1@gmail.com>
2024-12-04 20:35:47 +0300

    Refactor project structure and add domain events
    Updated Bookify.sln to change project path and GUIDs. Added domain event handling in Entity.cs. Updated User.cs to inherit from Entity and include new properties and methods. Added new domain event and value object files. Updated Bookify.Domain.csproj to include MediatR.Contracts package.

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,27 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
     public Guid Id { get; init; }
 
     protected Entity(Guid id)
     {
         id = Id;
     }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
 }
--- src/Bookify.Domain/Users/User.cs
+++ src/Bookify.Domain/Users/User.cs
@@ -1,5 +1,38 @@
-﻿namespace Bookify.Domain.Users;
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
+using Bookify.Domain.Users.ValueObjects;
 
-public class User
+namespace Bookify.Domain.Users;
+
+public sealed class User : Entity
 {
+    public FirstName FirstName { get;private set; }
+    public LastName LastName { get;private set; }
+    public Email Email { get;private set; }
+    private User(
+        Guid id, 
+        FirstName firstName,
+        LastName lastName,
+        Email email) 
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public static User Create(
+        FirstName firstName,
+        LastName lastName,
+        Email email)
+    {
+        var user = new User(
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email);
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
+
+        return user;
+    }
 }
